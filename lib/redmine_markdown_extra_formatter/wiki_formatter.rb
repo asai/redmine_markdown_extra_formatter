5ef6a84a948e572e5e03b1887c756f29ebd00404
Noboru Asai <noboru.asai@gmail.com>
2010-07-15 16:41:59 +0900

    update macro support for latest redmine

--- lib/redmine_markdown_extra_formatter/wiki_formatter.rb
+++ lib/redmine_markdown_extra_formatter/wiki_formatter.rb
@@ -100,40 +100,12 @@ module RedmineMarkdownExtraFormatter
     def to_html(&block)
       @macros_runner = block
       parsedText = BlueFeather.parse(@text)
-      parsedText = inline_macros(parsedText)
       parsedText = syntax_highlight(parsedText)
     rescue => e
       return("<pre>problem parsing wiki text: #{e.message}\n"+
              "original text: \n"+
              @text+
              "</pre>")
-    end
-
-    MACROS_RE = /
-          (!)?                        # escaping
-          (
-          \{\{                        # opening tag
-          ([\w]+)                     # macro name
-          (\(([^\}]*)\))?             # optional arguments
-          \}\}                        # closing tag
-          )
-        /x
-
-    def inline_macros(text)
-      text.gsub!(MACROS_RE) do
-        esc, all, macro = $1, $2, $3.downcase
-        args = ($5 || '').split(',').each(&:strip)
-        if esc.nil?
-          begin
-            @macros_runner.call(macro, args)
-          rescue => e
-            "<div class=\"flash error\">Error executing the <strong>#{macro}</strong> macro (#{e})</div>"
-          end || all
-        else
-          all
-        end
-      end
-      text
     end
 
     PreCodeClassBlockRegexp = %r{^<pre><code\s+class="(\w+)">\s*\n(.+?)</code></pre>}m
